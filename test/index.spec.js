3ea262471cffda408c0d97ecf7ffd22be33bd1cf
mohammadanang <m.anangnur@gmail.com>
2019-01-25 16:32:21 +0700

    update testing index

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,7 +2,8 @@ const expect = require('chai').expect;
 const server = require('../index');
 
 describe('TEST', () => {
-    it('should return a string', () => {
+    it('should return a string', (done) => {
         expect('CI with Travis').to.equal('CI with Travis');
+        done();
     });
 });
